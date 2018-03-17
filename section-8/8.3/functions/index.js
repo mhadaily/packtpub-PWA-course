b656ebd46af74de19a6a9cc72259b7cd8277bb19
Majid <mhadaily@gmail.com>
2018-03-17 13:02:53 +0100

    video 8.3

---
+++ section-8/8.3/functions/index.js
@@ -0,0 +1,62 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const webpush = require('web-push');
+const cors = require('cors')({
+  origin: true
+});
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+const serviceAccount = require("./note-pwa-packtpub-firebase-adminsdk-y13zc-a1aa90173b.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://note-pwa-packtpub.firebaseio.com"
+});
+
+exports.saveNote = functions.https.onRequest((request, response) => {
+  cors(request, response, () => {
+    admin.database().ref('notes').put({
+        id: request.body.id,
+        title: request.body.title,
+        note: request.body.note,
+        date: request.body.date,
+        synced: true,
+      })
+      .then(() => response.status(201).json({
+        message: 'Note saved!',
+        id: request.body.id
+      }))
+      .catch((err) => response.status(500).json({
+        error: err,
+        id: request.body.id,
+      }));
+  });
+});
+
+exports.deleteNote = functions.https.onRequest((request, response) => {
+  cors(request, response, () => {
+    admin.database().ref('notes/' + request.body.id)
+      .remove()
+      .then(() => response.status(200).json({
+        message: 'Note deleted!',
+        id: request.body.id
+      }))
+      .catch((err) => response.status(500).json({
+        error: err,
+        id: request.body.id,
+      }));
+  });
+});
+
+exports.saveSubscription = functions.https.onRequest((request, response) => {
+  cors(request, response, () => {
+    admin.database().ref('subscriptions').push(request.body)
+      .then(() => response.status(201).json({
+        message: 'Subscription granted!',
+        id: request.body.id
+      }))
+      .catch((err) => response.status(500).json({
+        error: err,
+        id: request.body.id,
+      }));
+  });
+});
